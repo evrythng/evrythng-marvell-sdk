dbb5ab164571b701b05ed61306d10de9dd5f5e5f
Ruslan Filipenko <ruslan.filipenko@evrythng.com>
2017-02-28 19:48:37 +0300

    add sub to properties (without habdling)

--- apps/demo/src/main.c
+++ apps/demo/src/main.c
@@ -266,6 +266,10 @@ static void evrythng_task()
     EvrythngSubThngAction(evt_handle, thng_id, "_led1", 0, action_led_callback);
     EvrythngSubThngAction(evt_handle, thng_id, "_led2", 0, action_led_callback);
 
+    EvrythngSubThngProperty(evt_handle, thng_id, "property_1", 0, action_led_callback);
+    EvrythngSubThngProperty(evt_handle, thng_id, "property_2", 0, action_led_callback);
+
+
     os_thread_create(&button1_thread, "button1_task", button_task, (void*)button_1, &button_stack, OS_PRIO_3);
     os_thread_create(&button2_thread, "button2_task", button_task, (void*)button_2, &button_stack, OS_PRIO_3);
 
